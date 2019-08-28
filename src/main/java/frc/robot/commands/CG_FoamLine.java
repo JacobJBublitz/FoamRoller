7aebd900c567a234bdeadf48cc652cbd19ad2916
Brianna Christiansen <christianseb@spu.edu>
2019-08-27 20:34:54 -0700

    added parallel

--- src/main/java/frc/robot/commands/CG_FoamLine.java
+++ src/main/java/frc/robot/commands/CG_FoamLine.java
@@ -4,10 +4,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class CG_FoamLine extends CommandGroup {
     public CG_FoamLine() {
+        addParallel(new CG_PressSequence());
         addSequential(new CG_OvenSequence());
         addSequential(new CG_FeedSequence(2.0));
 
-        addSequential(new CG_PressSequence());
-        addSequential(new CG_FeedSequence(2.0, false));
+        //addSequential(new CG_PressSequence());
+        //addSequential(new CG_FeedSequence(2.0, false));
     }
 }
