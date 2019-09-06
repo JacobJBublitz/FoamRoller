775f604127831d8f6b05730ee6b46b73c272d18b
Jacob Bublitz <jacob@bublitz.com>
2019-09-05 22:11:25 -0700

    Change oven time back to actual time

--- src/main/java/frc/robot/commands/CG_OvenSequence.java
+++ src/main/java/frc/robot/commands/CG_OvenSequence.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
 public class CG_OvenSequence extends CommandGroup {
     public CG_OvenSequence() {
         addSequential(new C_Oven(true));
-        addSequential(new WaitCommand(15)); // todo: 115
+        addSequential(new WaitCommand(115));
         addSequential(new C_Oven(false));
         addSequential(new WaitCommand(2));
     }
