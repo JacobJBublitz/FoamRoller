31d11bafc39e22a40af25e46ba62150b0808a8e4
Jacob Bublitz <jacob@bublitz.com>
2019-09-10 19:10:45 -0700

    Move Has Paper to status tab

--- src/main/java/frc/robot/subsystems/SS_Oven.java
+++ src/main/java/frc/robot/subsystems/SS_Oven.java
@@ -1,19 +1,33 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 
 public class SS_Oven extends Subsystem {
+    private static NetworkTableEntry hasPaperEntry;
+
     private final DoubleSolenoid ovenSolenoid = new DoubleSolenoid(RobotMap.OVEN_UP, RobotMap.OVEN_DOWN);
 
     private final DigitalInput paperDetector = new DigitalInput(RobotMap.OVEN_PAPER_DETECTOR);
 
+    static {
+        ShuffleboardTab tab = Shuffleboard.getTab("Status");
+
+        hasPaperEntry = tab.add("Has Paper", false)
+                .withWidget(BuiltInWidgets.kBooleanBox)
+                .getEntry();
+    }
+
     @Override
     public void periodic() {
-        SmartDashboard.putBoolean("Has Paper", isDetectingPaper());
+        hasPaperEntry.setBoolean(isDetectingPaper());
     }
 
     public void closeOven(boolean state) {
